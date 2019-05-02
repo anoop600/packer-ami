f1f409eb4e9dc2d0ac02f6988de0f8505958343d
anoop600 <anoop.jain10@gmail.com>
2019-05-02 14:04:22 +0000

    added files for jfrog artifactory

--- jenkins.sh
+++ jenkins.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-packer build ./jenkins/jenkinsPacker.json
+cd ./jenkins
+packer build jenkinsPacker.json
---
+++ jfrog/jfrogScript.sh
@@ -0,0 +1,10 @@
+sudo apt-get update -y
+sudo apt install openjdk-8-jdk -y
+
+sleep 5
+
+echo "JAVA is installed"
+
+sudo apt-get update && sudo apt-get install unzip
+wget https://s3-ap-southeast-1.amazonaws.com/jfrog-zip/jfrog-artifactory-oss-6.9.1.zip
+unzip jfrog-artifactory-oss-6.9.1.zip
