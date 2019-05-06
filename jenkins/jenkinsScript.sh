0f0e4249d14000cefd5f3d709d2e1e78f5787eb1
Neeraj Agarwal <45447725+NeerajAgarwal7@users.noreply.github.com>
2019-05-06 11:32:17 +0530

    Update jenkinsScript.sh

--- jenkins/jenkinsScript.sh
+++ jenkins/jenkinsScript.sh
@@ -1,4 +1,4 @@
-sudo apt-get update -y && sudo apt install openjdk-8-jdk -y
+sudo apt-get update && sudo apt install openjdk-8-jdk -y
 
 sleep 5
 
@@ -7,6 +7,6 @@ echo "JAVA is installed"
 
 echo -ne '\n' |  wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
 echo deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
-sudo apt-get update -y && sudo apt-get install jenkins -y
+sudo apt-get update && sudo apt-get install jenkins -y
 sudo systemctl start jenkins
 sudo ufw allow 8080
